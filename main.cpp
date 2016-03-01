c2e2723d5525d78b2a0d9f5d20244e776c8c75e8
Galorn <alexis.teston@ecam-strasbourg.eu>
2016-03-01 19:43:01 +0100

    Base code
    Base code that stores the random values in a dynamic table and displays
    it.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    // INITIALISATION DES VARIABLES
+    int rand(), donnees(0), initialisation(0), condition(0), cases(0);
+    vector<int> scores;
+
+    // INITIALISATION DU DEPART DU RANDOM
+    srand(time(NULL));
+    // INTRODUCTION
+    cout << "Hello please enter the number of scores you want to sort with a merge sort" << endl;
+    cin >> initialisation;
+    cout << "You chose to enter " << initialisation << " scores." << endl ;
+
+    for(condition = initialisation; condition >0; condition--)
+    {
+        donnees = rand();
+        scores.push_back(donnees);
+        cases++ ;
+        cout << "coucou case " << cases << " il y a " << scores[condition] << endl;
+
+    }
+
+
+
     return 0;
 }
